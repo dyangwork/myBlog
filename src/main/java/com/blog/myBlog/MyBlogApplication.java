41eefbcbc55118b088b590a98c56a6a888e8b2b2
dyangwork <dyangwork@126.com>
2019-04-26 16:19:08 +0800

    登录页面样式调整提交

--- src/main/java/com/blog/myBlog/MyBlogApplication.java
+++ src/main/java/com/blog/myBlog/MyBlogApplication.java
@@ -1,9 +1,12 @@
 package com.blog.myBlog;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@AutoConfigurationPackage
 public class MyBlogApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/blog/myBlog/controller/LoginController.java
+++ src/main/java/com/blog/myBlog/controller/LoginController.java
@@ -1,13 +1,18 @@
 package com.blog.myBlog.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.Map;
+
+
+@Controller
 public class LoginController {
 
 	@RequestMapping("/login")
-	public String login(){
-		return "login";
+	public String login(Map<String,Object> map){
+		map.put("aaaa","sssss");
+		return "admin/login/login";
 	}
+
 }
